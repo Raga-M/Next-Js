1f70261ae43b0499fcd7ca9c0a2f67033c9c2cc6
Raga-M <ragavendran200@gmail.com>
2024-01-06 17:53:32 +0530

    unmatch route

---
+++ first-nextjs-app/app/dashboard/@dailyStatus/default.js
@@ -0,0 +1,9 @@
+import Card from "@/app/component/Card";
+
+function DefaultDailyStatus() {
+  return (
+ <Card>DailyStatus</Card>
+  )
+}
+
+export default DefaultDailyStatus;
---
+++ first-nextjs-app/app/dashboard/@notification/default.js
@@ -0,0 +1,10 @@
+import Card from "@/app/component/Card"
+
+function DefaultNotification() {
+    return (
+  <Card>Notification</Card>
+  
+        )
+  }
+  
+  export default  DefaultNotification
